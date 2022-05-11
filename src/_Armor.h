7a9091e6d7f46a96ffd5abb1cb7623174cb00b48
MrMarvinColex <91114928+MrMarvinColex@users.noreply.github.com>
2022-05-11 17:08:24 +0300

    -V1.3, update codstyle

--- src/_Armor.h
+++ src/_Armor.h
@@ -8,25 +8,24 @@
 #include <string>
 
 class ArmorUnit {
-public:
-	ArmorUnit() = default;
-	ArmorUnit (int value) : name(), durability(100), protectionFactor(value) {}
+ public:
+  ArmorUnit() = default;
+  ArmorUnit (int value) : name(), durability(100), protectionFactor(value) {}
 
-public:
-	std::string name;
-	int durability;
-	double protectionFactor;
+ public:
+  std::string name;
+  int durability;
+  double protectionFactor;
 };
 
 class Armor {
-public:
-	Armor() = default;
-	Armor (int value) : helmet(value), chestplate(value), leggins(value), boots(value) {}
+ public:
+  Armor() = default;
+  Armor (int value) : helmet(value), chestplate(value), leggins(value), boots(value) {}
 
-public:
-	ArmorUnit helmet;
-	ArmorUnit chestplate;
-	ArmorUnit leggins;
-	ArmorUnit boots;
+ public:
+  ArmorUnit helmet;
+  ArmorUnit chestplate;
+  ArmorUnit leggins;
+  ArmorUnit boots;
 };
-

ee139094f9efa973d64ae09eaad4ae64e546851e
MrMarvinColex <91114928+MrMarvinColex@users.noreply.github.com>
2022-05-11 17:09:55 +0300

    -V1.3, update codstyle

--- src/_Items.h
+++ src/_Items.h
@@ -9,40 +9,40 @@
 #include <vector>
 
 class monetaryUnit {
-public:
-	monetaryUnit() = default;
-	monetaryUnit (std::string name, int nominal) : nameOfCurrency(name), nominal(nominal) {}
+ public:
+  monetaryUnit() = default;
+  monetaryUnit (std::string name, int nominal) : nameOfCurrency(name), nominal(nominal) {}
 
-	std::string getName() {
-                return nameOfCurrency;
-        }
-        int getNominal() {
-                return nominal;
-        }
+  std::string getName() {
+    return nameOfCurrency;
+  }
+  int getNominal() {
+    return nominal;
+  }
 
-private:
-        std::string nameOfCurrency;
-        int nominal;
+ private:
+  std::string nameOfCurrency;
+  int nominal;
 };
 
 class Wallet {
-public:
-	Wallet (int num = 0) : money() {
-		if (!num)
-			money = {};
-		else {
-			money = {{monetaryUnit("gold", 1), num}};
-		}
-	}
+ public:
+  Wallet (int num = 0) : money() {
+    if (!num)
+      money = {};
+    else {
+      money = {{monetaryUnit("gold", 1), num}};
+    }
+  }
 
-	std::pair<std::string, int> getMoney() {
-                return {money[0].first.getName(), money[0].first.getNominal() * money[0].second};
-        }
-        void decreaseMoney(int cost) {
-                money[0].second -= cost;
-        }
+  std::pair<std::string, int> getMoney() {
+    return {money[0].first.getName(), money[0].first.getNominal() * money[0].second};
+  }
+  void decreaseMoney(int cost) {
+    money[0].second -= cost;
+  }
 
-private:
-        std::vector<std::pair<monetaryUnit, int>> money;
+ private:
+  std::vector<std::pair<monetaryUnit, int>> money;
 };
 

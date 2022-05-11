a02ecaa3b5cd371c1e2ae2394d1f6788fa5592fd
MrMarvinColex <91114928+MrMarvinColex@users.noreply.github.com>
2022-05-11 17:10:22 +0300

    -V1.3, update codstyle

--- src/_Weapon.h
+++ src/_Weapon.h
@@ -8,18 +8,18 @@
 #include <string>
 
 class Weapon {
-public:
-	Weapon() = default;
-	Weapon (std::string name, int damage) : name(name), damage(damage) {}
+ public:
+  Weapon() = default;
+  Weapon (std::string name, int damage) : name(name), damage(damage) {}
 
-public:
-	std::string name;
-	int damage;
+ public:
+  std::string name;
+  int damage;
 
-	int getDamage() {
-		return damage;
-	}
-	std::string getName() {
-		return name;
-	}
+  int getDamage() {
+    return damage;
+  }
+  std::string getName() {
+    return name;
+  }
 };

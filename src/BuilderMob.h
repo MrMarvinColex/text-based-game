7f4a19c381ff25879974645c3a039b00383f30f2
MrMarvinColex <91114928+MrMarvinColex@users.noreply.github.com>
2022-05-11 17:06:38 +0300

    -V1.3, update codstyle

--- src/BuilderMob.h
+++ src/BuilderMob.h
@@ -13,71 +13,69 @@
 #include "_Weapon.h"
 
 class Builder {
-public:
-	virtual ~Builder() {}
-	// virtual void reset() const = 0;
-	virtual void setHealth() const = 0;
-	virtual void setInventory() const = 0;
-	virtual void setArmor() const = 0;
-	virtual void setWeapon() const = 0;
+ public:
+  virtual ~Builder() {}
+  virtual void setHealth() const = 0;
+  virtual void setInventory() const = 0;
+  virtual void setArmor() const = 0;
+  virtual void setWeapon() const = 0;
 };
 
 class Beast : public Builder {
-private:	
-	Mob* mob;
+ private:
+  Mob* mob;
 
-public:
-	// constructor
-	Beast() {
-		reset();
-	}
-	// destructor
-	~Beast() {
-		delete mob;
-	}
-	
-	// creating method
-	void reset() {
-		mob = new Mob();
-	}
+ public:
+  // constructor
+  Beast() {
+    reset();
+  }
+  // destructor
+  ~Beast() {
+    delete mob;
+  }
 
-	// building methods
-	void setHealth() const override {
-		mob->healthValue = 21 + rand() % 10;
-	}
-	void setInventory() const override {
-		mob->mobName = "Beast";
-		mob->inventory = 1 + rand() % 11;
-	}
-	void setArmor() const override {
-		mob->armor = 4 + rand() % 2;
-	}
-	void setWeapon() const override {
-		int damage = 3 + rand() % 5;
-		mob->weapon = Weapon("claw", damage);
-	}
+  // creating method
+  void reset() {
+    mob = new Mob();
+  }
 
-	// get product
-	Mob* getProduct() {
-		Mob* result = mob;
-		reset();
-		return result;
-	}
+  // building methods
+  void setHealth() const override {
+    mob->healthValue = 21 + rand() % 10;
+  }
+  void setInventory() const override {
+    mob->mobName = "Beast";
+    mob->inventory = 1 + rand() % 11;
+  }
+  void setArmor() const override {
+    mob->armor = 4 + rand() % 2;
+  }
+  void setWeapon() const override {
+    int damage = 3 + rand() % 5;
+    mob->weapon = Weapon("claw", damage);
+  }
+
+  // get product
+  Mob* getProduct() {
+    Mob* result = mob;
+    reset();
+    return result;
+  }
 };
 
 class Director {
-private:
-	Builder* builder;
+ private:
+  Builder* builder;
 
-public:
-	void setBuilder(Builder* transferedBuilder) {
-		builder = transferedBuilder;
-	}
-	void buildMob() {
-		builder->setHealth();
-		builder->setInventory();
-		builder->setArmor();
-		builder->setWeapon();
-	}
+ public:
+  void setBuilder(Builder* transferedBuilder) {
+    builder = transferedBuilder;
+  }
+  void buildMob() {
+    builder->setHealth();
+    builder->setInventory();
+    builder->setArmor();
+    builder->setWeapon();
+  }
 };
-
